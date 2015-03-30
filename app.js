306f171ebb70fa1b0e8deac30da93c1d76e50a63
simon19940929 <102306076@nccu.edu.tw>
2015-03-30 23:45:10 +0800

    Update app.js

--- app.js
+++ app.js
@@ -59,7 +59,6 @@ $(function(){
 		}
 
 		if (currentText!=''&&temp==''&&operator==''&&equal!='') {
-			// alert('C4');
 			equal='';
 			currentText='4';
 			operator='';
@@ -76,8 +75,7 @@ $(function(){
 		}
 
 		if (currentText!=''&&temp==''&&operator==''&&equal!='') {
-			// alert('C4');
-			equal='';
+				equal='';
 			currentText='5';
 			operator='';
 			$('#message').text(currentText);
@@ -93,7 +91,7 @@ $(function(){
 		}
 
 		if (currentText!=''&&temp==''&&operator==''&&equal!='') {
-			// alert('C4');
+		
 			equal='';
 			currentText='6';
 			operator='';
@@ -110,7 +108,7 @@ $(function(){
 		}
 
 		if (currentText!=''&&temp==''&&operator==''&&equal!='') {
-			// alert('C4');
+
 			equal='';
 			currentText='7';
 			operator='';
@@ -127,7 +125,7 @@ $(function(){
 		}
 
 		if (currentText!=''&&temp==''&&operator==''&&equal!='') {
-			// alert('C4');
+
 			equal='';
 			currentText='8';
 			operator='';
@@ -144,7 +142,7 @@ $(function(){
 		}
 
 		if (currentText!=''&&temp==''&&operator==''&&equal!='') {
-			// alert('C4');
+
 			equal='';
 			currentText='9';
 			operator='';
@@ -165,28 +163,28 @@ $(function(){
 	$('#btn0').on('click',function(){
 		if(currentText==''&&temp==''&&operator==''&&equal==''||currentText=='0'){
 			currentText='0'
-			// alert('C1');
+
 		}else if (currentText!=''&&temp==''&&operator==''&&equal=='') {
-			// alert('C2');
+
 			currentText += '0';
 			$('#message').text(currentText);
 		}else if (currentText==''&&temp!=''&&operator!=''&&equal==''){
-			// alert('C3');
+
 			currentText += '0';
 			$('#message').text(currentText);
 		}else if (currentText!=''&&temp==''&&operator==''&&equal!='') {
-			// alert('C4');
+	
 			equal='';
 			currentText='0';
 			operator='';
 			$('#message').text(currentText);
 			currentText='';
 		}else if (currentText!=''&&temp!=''&&operator!=''&&equal==''){
-			// alert('C5');
+		
 			currentText += '0';
 			$('#message').text(currentText);
 		}else{
-			// alert('C6');
+	
 			currentText += '0';
 			$('#message').text(currentText);
 
@@ -233,22 +231,22 @@ $(function(){
 		operator = '';
 	})
 
-	function calculate(num1,num2,operator){
+	function calculate(n1,n2,operator){
 		var result;
-		num1 = parseInt(num1);
-		num2 = parseInt(num2);
+		n1 = parseInt(n1);
+		n2 = parseInt(n2);
 		switch (operator){
 			case '+':
-				result=num1+num2;
+				result=n1+n2;
 				break;
 			case '-':
-			    result=num1-num2;
+			    result=n1-n2;
 			    break;
 			case '*':
-			    result=num1*num2;
+			    result=nu*n2;
 			    break;
             case '/':
-			    result=num1/num2;
+			    result=n1/n2;
 			    break;
 			default:
 				return;
